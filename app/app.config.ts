a56ece2da92a4bf58033b4ba5b0d322df97588f9
Nguyen Ngoc Long <43560378+nguyenngoclongdev@users.noreply.github.com>
2026-01-07 14:29:08 +0700

    Update contact and social links in app config

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,11 +1,11 @@
 export default defineAppConfig({
   title: 'Sink',
-  email: 'sink.cool@miantiao.me',
-  github: 'https://github.com/miantiao-me/sink',
-  twitter: 'https://sink.cool/kai',
-  telegram: 'https://sink.cool/telegram',
-  mastodon: 'https://sink.cool/mastodon',
-  blog: 'https://sink.cool/blog',
+  email: 'sink@149159.xyz',
+  github: 'https://link.149159.xyz/github',
+  twitter: 'https://link.149159.xyz/kai',
+  telegram: 'https://link.149159.xyz/telegram',
+  mastodon: 'https://link.149159.xyz/mastodon',
+  blog: 'https://link.149159.xyz/blog',
   description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
   image: 'https://sink.cool/banner.png',
   previewTTL: 300, // 5 minutes
